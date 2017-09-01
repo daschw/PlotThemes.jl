08bd35bb38fbb99120d9b6fc5d68aea64a60307b
Daniel Schwabeneder <daniel.schwabeneder@openmailbox.org>
2017-09-01 17:03:39 +0200

    some adjustments

--- src/tuwien.jl
+++ src/tuwien.jl
@@ -1,18 +1,18 @@
 
-const myblue = RGB(0.0, 0.4, 0.6)
-const myred = RGB(0.6, 0.2, 0.2)
-const mygray = RGB(0.25, 0.25, 0.25)
+const tublue = RGB(0.0, 0.4, 0.6)
+const tured = RGB(0.6, 0.1, 0.1)
+const tugray = RGB(0.25, 0.25, 0.25)
 
 tu_palette = [
-    myblue,
-    myred
+    tublue,
+    tured,
 ]
 
 _themes[:tuwien] = PlotTheme(
     colorant"white",
     colorant"white",
-    mygray,
-    mygray,
+    tugray,
+    tugray,
     expand_palette(colorant"white", tu_palette; lchoices=linspace(57,57,1), cchoices=linspace(100,100,1)),
     julia_palette[[1, 2]]
 )
